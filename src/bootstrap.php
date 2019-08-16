f46a9321d2e7b4fd002480b2915a9e88092371dc
Milad Nekofar <milad@nekofar.com>
2019-08-16 19:04:49 +0430

    Add improvements to the directory structure

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
 use Siler\GraphQL;
 use Siler\Http\Request;
 use Siler\Http\Response;
 
-require 'vendor/autoload.php';
+// Loads environment variables
+Dotenv::create( '../')->load();
 
 // Enable CORS
 Response\header('Access-Control-Allow-Origin', '*');
@@ -13,10 +15,10 @@ Response\header('Access-Control-Allow-Headers', 'content-type');
 // Respond only for POST requests
 if (Request\method_is('post')) {
     // Context
-    $context['loaders'] = include __DIR__ . '/loaders.php';
+    $context['loaders'] = include 'loaders.php';
 
     // Retrieve the Schema
-    $schema = include __DIR__ . '/schema.php';
+    $schema = include 'schema.php';
 
     // Give it to Siler
     GraphQL\init($schema, null, $context);
